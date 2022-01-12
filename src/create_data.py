1a6f74280ea8aa23244c0a3cefb52575623cfa8a
Nico Nowak (alias Winzu) <50320269+WinzuDarkpaw@users.noreply.github.com>
2022-01-12 12:52:40 +0100

    Data creation and .xml

---
+++ src/create_data.py
@@ -0,0 +1,57 @@
+# imports of needed libraries
+import cv2, sys, numpy, os
+
+###########################################################################################################################
+# LOCAL VARIABLES TO CHANGE FOR NEEDS #####################################################################################
+###########################################################################################################################
+# specifies the dimensions of the camera
+(width, height) = (130, 100)
+
+# specifies the method how faces are recognized
+face_classificator = 'haar_cascade_frontal.xml'
+project_path = 'C:\\Users\\bgt.nowanico\\Desktop\\GigaCHAD\\GigaCHAD-main\\src'
+datasets = 'datasets'
+sub_data = 'videodata'
+
+###########################################################################################################################
+# ################################### #####################################################################################
+###########################################################################################################################
+
+####################################################################################
+def create_dataset_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print("Dataset directory created.")
+    else:
+        print("Dataset directory already exists.")
+####################################################################################
+####################################################################################
+def create_image_from_webcam(path, webcam, width, height):
+    (_, image) = webcam.read()
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+
+    # draw a rectangle around the recognized face
+    for(xCell, yCell, faceWidth, faceHeight) in faces:
+        cv2.rectangle(image, (xCell, yCell), (xCell + faceWidth, yCell + faceHeight), (255, 0, 0), 2)
+        face = gray[yCell:yCell + faceHeight, xCell:xCell + faceWidth]
+        face_resize = cv2.resize(face, (width, height))
+        cv2.imwrite('% s/% s.png' % (dataset_folder_path, count), face_resize)
+####################################################################################
+
+dataset_folder_path = os.path.join(project_path, datasets, sub_data)
+create_dataset_directory(dataset_folder_path)
+
+face_cascade = cv2.CascadeClassifier(face_classificator)
+webcam = cv2.VideoCapture(0)
+
+count = 1
+while count < 30:
+    create_image_from_webcam(dataset_folder_path, webcam, width, height)
+    count += 1
+
+    cv2.imshow('OpenCV', image)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+
---
+++ src/face_recognition.py
@@ -0,0 +1,6 @@
+# imports of needed libraries
+import cv2, sys, numpy, os
+
+# specifies the method how faces are recognized
+face_classificator = 'haar_cascade_frontal.xml'
+datasets = 'datasets'
