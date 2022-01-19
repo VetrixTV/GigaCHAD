f82efac3f3baa7275a1055a84945a089b3ea2ac7
Wilhelm Adam <wilhelm.adam@sla.de>
2022-01-19 09:22:57 +0100

    added something

--- Arduino/src/stepper/stepper.ino
+++ Arduino/src/stepper/stepper.ino
@@ -1,11 +1,19 @@
 #include <Stepper.h>
 int SPU = 2048;
-Stepper Motor(SPU, 3,5,4,6);
+Stepper Motor(SPU, 3, 5, 4, 6);
+
+int stepCount = 0;
+
 void setup()
 {
-Motor.setSpeed(5);
+  Motor.setSpeed(5);
+  Serial.begin(9600);
 }
-void loop() 
+void loop()
 {
-Motor.step(1);
+ if(true){
+  Motor.step(1);
+ } else {
+  Motor.step(-1);
+ }
 }
