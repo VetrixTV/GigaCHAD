f4df8ba909aea3189937217f814031cae6e82ba5
Wilhelm Adam <wilhelm.adam@sla.de>
2022-01-19 11:57:16 +0100

    added the switch case

--- Arduino/src/stepper/stepper.ino
+++ Arduino/src/stepper/stepper.ino
@@ -1,19 +1,40 @@
 #include <Stepper.h>
 int SPU = 2048;
-Stepper Motor(SPU, 3, 5, 4, 6);
+Stepper MotorX(SPU, 3, 5, 4, 6);
+Stepper MotorY(SPU, 9, 10, 11, 8);
 
-int stepCount = 0;
-
+char input;
 void setup()
 {
-  Motor.setSpeed(5);
+  MotorX.setSpeed(5);
+  MotorY.setSpeed(5);
   Serial.begin(9600);
 }
 void loop()
 {
- if(true){
-  Motor.step(1);
- } else {
-  Motor.step(-1);
- }
+  while (!Serial.available());
+  input = Serial.readString().charAt(0);
+
+  switch (input) {
+    case 'U':
+      MotorY.step(1);
+      delay(2000);
+      break;
+    case 'D':
+      MotorY.step(-1);
+      delay(2000);
+      break;
+    case 'R':
+      MotorX.step(1);
+      delay(2000);
+      break;
+    case 'L':
+      MotorX.step(-1);
+      delay(2000);
+      break;
+    default:
+      Serial.println("Whatever is: ");
+      Serial.println(input);
+      break;
+  }
 }
