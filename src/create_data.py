7a757de0e2aa7f12eb64a2daa537c1ec466c97d0
Nico Nowak (alias Winzu) <50320269+WinzuDarkpaw@users.noreply.github.com>
2022-01-19 09:22:28 +0100

    xCell, yCell werden ausgegeben

--- src/create_data.py
+++ src/create_data.py
@@ -1,5 +1,5 @@
 # imports of needed libraries
-import cv2, sys, numpy, os
+import cv2, sys, numpy, os, csv
 
 ###########################################################################################################################
 # LOCAL VARIABLES TO CHANGE FOR NEEDS #####################################################################################
@@ -33,17 +33,24 @@ face_cascade = cv2.CascadeClassifier(face_classificator)
 webcam = cv2.VideoCapture(0)
 
 count = 1
-while count < 30:
+while count < 2147483647:
     (_, image) = webcam.read()
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
 
     # draw a rectangle around the recognized face
     for(xCell, yCell, faceWidth, faceHeight) in faces:
-        cv2.rectangle(image, (xCell, yCell), (xCell + faceWidth, yCell + faceHeight), (255, 0, 0), 2)
-        face = gray[yCell:yCell + faceHeight, xCell:xCell + faceWidth]
-        face_resize = cv2.resize(face, (width, height))
-        cv2.imwrite('% s/% s.png' % (dataset_folder_path, count), face_resize)
+        if count % 600:
+            print('Coordinates will get saved: ', xCell, yCell)
+            cv2.rectangle(image, (xCell, yCell), (xCell + faceWidth, yCell + faceHeight), (255, 0, 0), 2)
+            face = gray[yCell:yCell + faceHeight, xCell:xCell + faceWidth]
+            face_resize = cv2.resize(face, (width, height))
+            cv2.imwrite('% s/% s.png' % (dataset_folder_path, count), face_resize)
+        else
+            cv2.rectangle(image, (xCell, yCell), (xCell + faceWidth, yCell + faceHeight), (255, 0, 0), 2)
+            face = gray[yCell:yCell + faceHeight, xCell:xCell + faceWidth]
+            face_resize = cv2.resize(face, (width, height))
+            cv2.imwrite('% s/% s.png' % (dataset_folder_path, count), face_resize)
     count += 1
 
     cv2.imshow('OpenCV', image)
