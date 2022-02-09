e963939f5cd1c4245a38b3e35281c7f64538fe19
Nico Nowak (alias Winzu) <50320269+WinzuDarkpaw@users.noreply.github.com>
2022-02-09 10:24:58 +0100

    Update create_data.py

--- src/create_data.py
+++ src/create_data.py
@@ -1,5 +1,5 @@
 # imports of needed libraries
-import cv2, sys, numpy, os, csv
+import cv2, sys, numpy, os, csv, time
 
 ###########################################################################################################################
 # LOCAL VARIABLES TO CHANGE FOR NEEDS #####################################################################################
@@ -40,14 +40,15 @@ while count < 2147483647:
 
     # draw a rectangle around the recognized face
     for(xCell, yCell, faceWidth, faceHeight) in faces:
-        with open(os.path.join(project_path, 'coordinates.csv'), 'r+') as csv:
-            csv.seek(0)
-            csv.truncate()
-            csv.write('X;Y\n')
-            csv.write(xCell, ';', yCell, '\n')
-            csv.close()
         if count % 3000:
             print('Coordinates will get saved: ', xCell, yCell)
+            with open(os.path.join(project_path, 'coordinates.csv'), 'r+') as csv:
+                csv.seek(0)
+                csv.truncate()
+                csv.write('X;Y\n')
+                csv.write(xCell, ';', yCell, '\n')
+                csv.close()
+                time.sleep(2000)
             cv2.rectangle(image, (xCell, yCell), (xCell + faceWidth, yCell + faceHeight), (255, 0, 0), 2)
             face = gray[yCell:yCell + faceHeight, xCell:xCell + faceWidth]
             face_resize = cv2.resize(face, (width, height))
@@ -58,7 +59,6 @@ while count < 2147483647:
             face_resize = cv2.resize(face, (width, height))
             cv2.imwrite('% s/% s.png' % (dataset_folder_path, count), face_resize)
     count += 1
-    sleep(2000)
     
     cv2.imshow('OpenCV', image)
     key = cv2.waitKey(10)
