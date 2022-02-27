504ca02d1e472e824a910dae793397ab3b199da5
Nico Nowak (alias Winzu) <50320269+WinzuDarkpaw@users.noreply.github.com>
2022-02-27 12:35:15 +0100

    Thresholds shown in camera

--- src/create_data.py
+++ src/create_data.py
@@ -1,9 +1,6 @@
 # imports of needed libraries
 import cv2, sys, numpy, os, csv, time
 
-###########################################################################################################################
-# LOCAL VARIABLES TO CHANGE FOR NEEDS #####################################################################################
-###########################################################################################################################
 # specifies the dimensions of the camera
 (width, height) = (1920, 1080)
 
@@ -12,24 +9,17 @@ face_classificator = 'haar_cascade_frontal.xml'
 project_path = os.path.curdir
 datasets = 'datasets'
 sub_data = 'videodata'
+dataset_folder_path = os.path.join(project_path, datasets, sub_data)
+create_dataset_directory(dataset_folder_path)
+csv_file_path = os.path.join(project_path, 'coordinates.csv')
 
-###########################################################################################################################
-# ################################### #####################################################################################
-###########################################################################################################################
-
-####################################################################################
 def create_dataset_directory(path):
     if not os.path.exists(path):
         os.makedirs(path)
         print("Dataset directory created.")
     else:
         print("Dataset directory already exists.")
-####################################################################################
 
-dataset_folder_path = os.path.join(project_path, datasets, sub_data)
-create_dataset_directory(dataset_folder_path)
-
-csv_file_path = os.path.join(project_path, 'coordinates.csv')
 
 if not os.path.exists(csv_file_path):
     csv_create = open(os.path.join(project_path, 'coordinates.csv'), 'x')
@@ -41,6 +31,8 @@ webcam = cv2.VideoCapture(0)
 count = 1
 while count < 2147483647:
     (_, image) = webcam.read()
+    cv.rectangle(image, (0,140), (260,180), (255,0,0), 2)
+    cv.rectangle(image, (190,0), (250,160), (255, 0, 0), 2)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.3, 4)
 
@@ -54,9 +46,6 @@ while count < 2147483647:
                     csv.truncate()
                     csv.write(str(xCell) + ';' + str(yCell) + '\n')
                     csv.close()
-                # Hier muss Leonardo die Datei auslesen!
-                # Mittelpunkt des Bildes bei: X=260 Y=160 (Bei Laptop-Kamera, bei der Amazon-Kamera müssen wir die Mittleren Koordinaten ermitteln)
-                # VIEL SPAß LEONARDO :)
             except IOError:
                 print('File is already opened')
             cv2.rectangle(image, (xCell, yCell), (xCell + faceWidth, yCell + faceHeight), (255, 0, 0), 2)
